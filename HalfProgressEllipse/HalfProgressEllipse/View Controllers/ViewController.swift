bb23dfb8a4f69bf0d027e4a8c4e07cdb7d8c2f98
Alexandra Rhodes <anrhodes5738@gmail.com>
2020-05-17 13:44:02 -0500

    not functioning + working out bugs

--- HalfProgressEllipse/HalfProgressEllipse/View Controllers/ViewController.swift
+++ HalfProgressEllipse/HalfProgressEllipse/View Controllers/ViewController.swift
@@ -9,12 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var progressBar: ARHalfEllipseProgressView!
+    var timer: Timer!
+    var progressCounter:Float = 0
+    let duration:Float = 10.0
+    var progressIncrement:Float = 0
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let progressView = ARHalfEllipseProgressView()
+        progressView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(progressView)
+        self.progressBar = progressView
+        
+        progressView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50).isActive = true
+        progressView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50).isActive = true
+        progressView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+
+        progressIncrement = 1.0/duration
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.showProgress), userInfo: nil, repeats: true)
+        
     }
 
 
+    @objc func showProgress() {
+       if(progressCounter > 1.0){timer.invalidate()}
+       progressBar.progress = progressCounter
+       progressCounter = progressCounter + progressIncrement
+       }
 }
 
