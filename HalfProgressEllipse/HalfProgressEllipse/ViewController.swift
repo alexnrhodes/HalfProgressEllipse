0e7ae47892b601eba9996342c8b3cb3152afd586
Alexandra Rhodes <anrhodes5738@gmail.com>
2020-05-16 20:59:58 -0500

    add Xcode project

---
+++ HalfProgressEllipse/HalfProgressEllipse/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HalfProgressEllipse
+//
+//  Created by Alex Rhodes on 5/16/20.
+//  Copyright © 2020 Alex Rhodes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
